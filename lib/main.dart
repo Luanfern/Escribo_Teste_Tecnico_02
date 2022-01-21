5849c9651bd8b1ade223752cdee88f58fecce9e9
LuanPegador07 <51713644+Luanfern@users.noreply.github.com>
2022-01-21 11:42:07 -0300

    correctmain

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
-import 'package:snakesandladders_escribo/Provider/providergame.dart';
 import 'package:snakesandladders_escribo/views/Game.dart';
-import 'package:snakesandladders_escribo/views/Lobby.dart';
+import 'Provider/providergame.dart';
 
 void main() => runApp(
       MaterialApp(
